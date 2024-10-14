dd9ee378fbcb9eb6017c3c94b4912cec27763279
Aanish Ramzan <110470875+aanish27@users.noreply.github.com>
2024-10-14 11:47:40 -0400

    label on hover added

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,26 +3,26 @@
        <nav class="nav">
             <div>
               <a href="" class="nav_logo" >
-                <i class='bi bi-arrow-left-right nav_logo-icon'></i>
-                <span class="nav_logo-name">Vista G</span>
+                <i title="Vista G" class='bi bi-arrow-left-right nav_logo-icon'></i>
+                <span class="nav_logo-name" >Vista G</span>
                 </a>
 
                 <div class="nav_list">
-                    <a href="{{ route('companys.index') }}" class="nav_link active" id="btn-company">
+                    <a title="Company" href="{{ route('companys.index') }}" class="nav_link active" id="btn-company">
                     <i class="bi bi-building  nav-icon"></i>
                         <span class="nav_name">Company</span>
                     </a>
                 </div>
 
                  <div class="nav_list">
-                    <a href=" {{ route('employee') }} " class="nav_link" id="btn-employee">
+                    <a title="Employee" href=" {{ route('employee') }} " class="nav_link" id="btn-employee">
                     <i class="bi bi-person-lines-fill  nav-icon"></i>
                         <span class="nav_name">Employee</span>
                     </a>
                 </div>
 
                  <div class="nav_list">
-                    <a href="#" class="nav_link ">
+                    <a  title="Dashboard" href="#" class="nav_link ">
                     <i class="bi bi-diagram-3  nav-icon"></i>
                         <span class="nav_name">Dashboard</span>
                     </a>
