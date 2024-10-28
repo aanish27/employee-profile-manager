2843af70a128eb340423d19a00db55d8d154fc7f
Aanish Ramzan <110470875+aanish27@users.noreply.github.com>
2024-10-28 12:06:27 -0400

    svgicon replaced to webfont-icon

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,23 +1,15 @@
 <nav class="navbar w-100  py-1 ps-2 pe-3 bg-white shadow-sm" >
-    <div class="nav-left d-flex align-items-center gap-2">
+    <div class="nav-left d-flex align-items-center gap-2 justify-content-center">
         <button id="sidebar-toggle"  href="" class="btn btn-nav m-0 p-0">
-            <svg xmlns="http://www.w3.org/2000/svg"  width="20" height="20" fill="currentColor" class="bi bi-justify" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/>
-            </svg>
+            <i class="bi bi-justify fs-5 "></i>
         </button>
-        <h1 class="fs-5" >{{ $title }}</h1>
+        <h1 class="fs-5 my-0" >{{ $title }}</h1>
     </div>
 
     <div class="nav-right d-flex align-items-center gap-2 ">
-        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="rounded-5 border-black border-1" title="Logout">
-            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#022658" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
-                <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
-            </svg>
-        </a>
+        <i class="bi bi-box-arrow-right fs-5 "></i>
         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
             @csrf
         </form>
-
     </div>
 </nav>
