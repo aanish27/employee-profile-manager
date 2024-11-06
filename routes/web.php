737b891ae3b1d0292b8650abae3976fa8da54d65
Aanish Ramzan <110470875+aanish27@users.noreply.github.com>
2024-11-06 15:13:18 -0500

    Revert "dashboard file removed"
    This reverts commit 953f01907b5125fde33e727b0c7d3c7548201a39.

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,26 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CompanyController;
 
+
+// Route::get('/', function () {return view('admin');})->name('index')->middleware('auth');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
 Route::middleware(['auth', 'auth:sanctum'])->group(function () {
     Route::get('company/draw', [CompanyController::class, 'draw'])->name('company.draw');
     Route::resource('companys', CompanyController::class);
 });
+
+
 
 require __DIR__.'/auth.php';
 require __DIR__.'/api.php';
