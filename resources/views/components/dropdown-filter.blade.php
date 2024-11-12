3a9f14e5aa8ad3c7eb1145f550947885733c9832
Aanish Ramzan <110470875+aanish27@users.noreply.github.com>
2024-11-12 17:34:13 -0500

    select and label seperated

--- resources/views/components/dropdown-filter.blade.php
+++ resources/views/components/dropdown-filter.blade.php
@@ -1,15 +1,14 @@
 <div class="filter p-0" >
-    <label for="{{ $id }}" class="bi bi-filter"> {{ $name }}
-        <select id="{{ $id }}" class="col-1 select2-filter" style="width: {{ $width }}">
-            <option value="clear-btn">Clear Selection</option>
-            @foreach ( $collections  as $collection )
-            {{-- refactor this --}}
-                <option value="{{ $feild == "null" ? $collection : $collection->$feild}}" class="" >
-                    {{$feild == "null" ? $collection : $collection->$feild }}
-                </option>
-            @endforeach
-        </select>
-    </label>
+    <label for="{{ $id }}" class="bi bi-filter"> {{ $name }} </label>
+    <select id="{{ $id }}" class="col-1 select2-filter" style="width: {{ $width }}">
+        <option value="clear-btn">Clear Selection</option>
+        @foreach ( $collections  as $collection )
+        {{-- refactor this --}}
+            <option value="{{ $feild == "null" ? $collection : $collection->$feild}}" class="" >
+                {{$feild == "null" ? $collection : $collection->$feild }}
+            </option>
+        @endforeach
+    </select>
 </div>
 
 
