6d839a0f641a4cad4e53a749a3f05c63094cbe89
Aanish Ramzan <110470875+aanish27@users.noreply.github.com>
2024-11-14 16:45:58 -0500

    refactored to component layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,52 +1,29 @@
-@extends('layouts.guest')
-@section('main')
+<x-guest-layout>
+  <div class="form-signin w-25">
+      <form method="POST" action="{{ route('login') }}">
+          @csrf
+          <i class="bi bi-person-square mb-4 fs-1"></i>
+          <h1 class="h3 mb-3 fw-normal">Sign-In</h1>
 
-<main class="form-signin w-25" >
-  <style>
-    html , body{
-      height: 100%;
-    }
+          <div class="form-floating">
+            <input type="email" class="form-control floatingInput" id="email" name="email" placeholder="name@example.com" required autofocus autocomplete="username"  value="{{ env('APP_ENV') == 'local' ? 'admin@example.com' : '' }}">
+            <label for="email">Email</label>
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+          </div>
 
-    .form-signin .form-floating:focus-within {
-      z-index: 2;
-    }
+          <div class="form-floating">
+              <input type="password" class="form-control " id="password" name="password" placeholder="Password"  required autocomplete="current-password" value="{{ env('APP_ENV') == 'local' ? 'password' : '' }}" >
+              <label for="password">Password</label>
+              <x-input-error :messages="$errors->get('password')" class="mt-2" />
+          </div>
 
-    .form-signin input[type="email"] {
-      margin-bottom: -1px;
-      border-bottom-right-radius: 0;
-      border-bottom-left-radius: 0;
-    }
+          <div class="form-check text-start my-3">
+              <input class="form-check-input" type="checkbox" id="remember_me" name="remember">
+              <label class="form-check-label" for="remember_me">{{ __('Remember me') }}</label>
+          </div>
+          <button class="btn btn-primary w-100 py-2" type="submit">{{ __('Log in') }}</button>
+      </form>
+  </div>
+</x-guest-layout>
 
-    .form-signin input[type="password"] {
-      margin-bottom: 10px;
-      border-top-left-radius: 0;
-      border-top-right-radius: 0;
-    }
-  </style>
 
-  @section('title', 'Login')
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-        <i class="bi bi-person-square mb-4 fs-1"></i>
-        <h1 class="h3 mb-3 fw-normal">Sign-In</h1>
-
-        <div class="form-floating">
-          <input type="email" class="form-control floatingInput" id="email" name="email" placeholder="name@example.com" required autofocus autocomplete="username"  value="{{ env('APP_ENV') == 'local' ? 'admin@example.com' : '' }}">
-          <label for="email">Email</label>
-          <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="form-floating">
-            <input type="password" class="form-control " id="password" name="password" placeholder="Password"  required autocomplete="current-password" value="{{ env('APP_ENV') == 'local' ? 'password' : '' }}" >
-            <label for="password">Password</label>
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <div class="form-check text-start my-3">
-            <input class="form-check-input" type="checkbox" id="remember_me" name="remember">
-            <label class="form-check-label" for="remember_me">{{ __('Remember me') }}</label>
-        </div>
-        <button class="btn btn-primary w-100 py-2" type="submit">{{ __('Log in') }}</button>
-    </form>
-</main>
-@endsection
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,13 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>@yield('title')</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js' , 'resources/css/custom.sign-in.css'])
+        <title></title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-
-    <body class="container.fluid d-flex justify-content-center align-items-center">
-        @section('main')
-
-        @show
+    <body class="container-fluid">
+        <main class="d-flex justify-content-center align-items-center h-100 w-100" >
+            {{ $slot }}
+        </main>
     </body>
 </html>
