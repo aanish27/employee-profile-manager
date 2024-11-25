e04f169fd6befdc035a121968ce54d468a082217
Aanish Ramzan <110470875+aanish27@users.noreply.github.com>
2024-11-25 12:43:45 -0500

    design Tweaked

--- resources/views/emails/auth/reset-password.blade.php
+++ resources/views/emails/auth/reset-password.blade.php
@@ -1,12 +1,21 @@
 <x-mail::message>
-# Introduction
+# Please Take Action
 
-The body of your I DID IT.
+You are receiving this email because we received a password reset request for your account.
 
 <x-mail::button :url="$resetUrl">
-Button Text
+Reset Link
 </x-mail::button>
 
+**This password reset link will expire in 60 minutes From the time You received.**
+
+If you did not request a password reset, no further action is required.
+
 Thanks,<br>
-{{ config('app.name') }}
+**Support Team**
+
+<hr>
+
+If you're having trouble clicking the "Reset Password" button, copy and paste the URL below into your web browser:<{{ $resetUrl }}>
+
 </x-mail::message>
